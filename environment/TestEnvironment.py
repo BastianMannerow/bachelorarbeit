0f0c87ef64cbb992b2479e429a8a40b3a80ea602
Bastian Mannerow <bastian.mannerow@student.uni-luebeck.de>
2024-06-19 20:11:41 +0200

    Synchronising the two agents

--- environment/TestEnvironment.py
+++ environment/TestEnvironment.py
@@ -1,22 +1,21 @@
 import pyactr as actr
 
-class MatrixEnvironment(actr.Environment):
-    def __init__(self, width, height, focus_position):
-        super().__init__(focus_position=focus_position)
-        self.width = width
-        self.height = height
-        self.matrix = [[None for _ in range(width)] for _ in range(height)]
+# Initialisiere Umgebung und Stimuli
+stimuli = ['A', 'B', 'C']
+text = [
+    {1: {'text': stimuli[0], 'position': (100, 100)}},
+    {2: {'text': stimuli[1], 'position': (100, 100)}},
+    {3: {'text': stimuli[2], 'position': (100, 100)}}
+]
 
-    def update_position(self, agent, new_position):
-        for row in self.matrix:
-            for i in range(len(row)):
-                if row[i] == agent:
-                    row[i] = None
-        self.matrix[new_position[1]][new_position[0]] = agent
+environ = actr.Environment(focus_position=(100, 100))
+environproc = environ.environment_process
 
-    def print_matrix(self):
-        for row in self.matrix:
-            print(" ".join(["A" if cell is not None else "." for cell in row]))
-
-    def get_matrix(self):
-        return self.matrix
+# Chunk-Typen und Chunks definieren
+actr.chunktype("chunk", "value")
+actr.chunktype("read", "state")
+actr.chunktype("image", "img")
+actr.makechunk(nameofchunk="start", typename="chunk", value="start")
+actr.makechunk(nameofchunk="attend_let", typename="chunk", value="attend_let")
+actr.makechunk(nameofchunk="response", typename="chunk", value="response")
+actr.makechunk(nameofchunk="done", typename="chunk", value="done")
--- main.py
+++ main.py
@@ -1,106 +1,95 @@
-from agents import test
-from agents import test2
-from agents import test3
-from agents import test4
-from agents import test5
-import string
-import random
-import time
+"""
+Demo for multiple agents.
+
+This code shows how one can build an environment with multiple agents in which actions of each agent affects the environment for them both.
+
+There are two agents. AG1 should press keys A and C when those letters appear on the screen. AG2 should press B when that letter appears on the screen.
+
+Pressing the right key moves the screen from one letter to the next.
+
+pyactr was not designed with the functionality of supporting multiagents in mind. So the package has to be hacked a bit.
+
+First, sim.run() does not work. You have to go through events manually (see lines at while True how that is done). But that's fine, any serious modeling should do that, instead of using the running function.
+
+Second, in the two lines following the comment "# update stimulus for the other agent..." we have to go into the internal setup of event simulation and manually update the values there. It's not nice and it's likely it will break in more complicated cases. Please, use this code with caution.
+
+Third, there is some inherent asymmetry in the agents. If agent 1 cannot fire any rules whatsoever, the simulation would stop even if agent 2 has some actions ready. This could be avoided with some extra code (e.g., specify that break from the loop only happens after you check both/all agents).
+
+"""
+
+import simpy
+from agents import TestAgents
+
 import pyactr as actr
 
-from environment.TestEnvironment import MatrixEnvironment
+stimuli = ['A', 'B', 'C']
+text = [{1: {'text': stimuli[0], 'position': (100, 100)}}, {2: {'text': stimuli[1], 'position': (100, 100)}},
+        {3: {'text': stimuli[2], 'position': (100, 100)}}]
 
-"""
-agent = test.getAdditionAgent()
-agent2 = test2.getAdditionAgent()
+# environments
+environ = actr.Environment(focus_position=(100, 100))
+environproc = environ.environment_process
 
-# Start the first agent's simulation and run it
-x = agent.simulation()
-x.run()
+# import agents
+agent_one, agent_two = TestAgents.generate_agents(environ)
 
-print("------------------------------------------------")
+# We start the simulation for both agents
+agent1_sim = agent_one.simulation(realtime=False, environment_process=environproc, stimuli=text, triggers=stimuli,
+                                  times=3)
+agent2_sim = agent_two.simulation(realtime=False, environment_process=environproc, stimuli=text, triggers=stimuli,
+                                  times=3)
 
-# Start the second agent's simulation and run it
-y = agent2.simulation()
-y.run()
-"""
-print("------------------------------------------------")
-"""
-# Stimulus generieren
-stimulus = random.sample(string.ascii_uppercase, 1)[0]
-text = {1: {'text': stimulus, 'position': (100,100)}}
+# this is used for internal updates of environment
+old_stimulus1, old_stimulus2 = None, None
 
-# Environment erstellen
-environ = actr.Environment(focus_position=(100,100))
+# The following loop runs the whole simulation
 
-# Beide Agenten initialisieren
-m = test3.pressKey(environ)
-n = test3.pressKey(environ)
+while True:
 
-# Simulation für beide Agenten erstellen
-sim_m = m.simulation(realtime=True, environment_process=environ.environment_process, stimuli=text, triggers=stimulus, times=1)
-sim_n = n.simulation(realtime=True, environment_process=environ.environment_process, stimuli=text, triggers=stimulus, times=1)
+    # store current stimulus
+    try:
+        old_stimulus1 = agent1_sim._Simulation__env.stimulus.copy()
+    except AttributeError:
+        pass
 
-# Beide Simulationen gleichzeitig ausführen
-sim_m.run(1)
-sim_n.run(1)
-print("------------------------------------------------")
-"""
+    try:
+        # do one simulation step
+        agent1_sim.step()
 
-# Matrixgröße definieren
-MATRIX_WIDTH = 10
-MATRIX_HEIGHT = 5
+        # print event
+        print("AGENT1, ", agent1_sim.current_event)
 
-# Environment erstellen
-environ = MatrixEnvironment(MATRIX_WIDTH, MATRIX_HEIGHT, focus_position=(0, 2))
+        # update stimulus for the other agent if they changed due to the event agent 1 carried
+        if old_stimulus1 and old_stimulus1 != agent1_sim._Simulation__env.stimulus:
+            agent2_sim._Simulation__environment_activate.succeed(
+                value=(agent1_sim._Simulation__env.trigger, agent1_sim._Simulation__pr.env_interaction))
 
-# Beide Agenten initialisieren
-m = test4.pressKey(environ)
-n = test5.pressKey(environ)
-
-# Startpositionen der Agenten
-start_positions = [(0, 2), (0, 3)]
-environ.update_position(m, start_positions[0])
-environ.update_position(n, start_positions[1])
-
-# Simulation für beide Agenten erstellen
-sim_m = m.simulation(realtime=True, environment_process=environ.environment_process)
-sim_n = n.simulation(realtime=True, environment_process=environ.environment_process)
-
-# Funktion zum Bewegen der Agenten
-def move_agent(agent, position, key):
-    if key == 'w':
-        new_position = (position[0] + 1, position[1])
-        if new_position[0] < MATRIX_WIDTH:
-            environ.update_position(agent, new_position)
-            return new_position
-    return position
-
-# Beide Simulationen ausführen und die Bewegung der Agenten verfolgen
-positions = {m: start_positions[0], n: start_positions[1]}
-for _ in range(MATRIX_WIDTH):
-    # Simulationen laufen lassen
-    sim_m.run(1)
-    sim_n.run(1)
-
-    # Agenten bewegen
-    positions[m] = move_agent(m, positions[m], 'w')
-    positions[n] = move_agent(n, positions[n], 's')
-
-    # Matrix drucken
-    environ.print_matrix()
-
-    # Pause von einer Sekunde
-    time.sleep(1)
-
-    # Überprüfen, ob ein Agent das Ende erreicht hat
-    if positions[m][0] == MATRIX_WIDTH - 1 or positions[n][0] == MATRIX_WIDTH - 1:
+    # if the schedule is empty, the agent has no rules to do and the action is stopped
+    except simpy.core.EmptySchedule:
         break
 
-# Gewinner bestimmen
-if positions[m][0] == MATRIX_WIDTH - 1:
-    print("Agent m hat gewonnen!")
-elif positions[n][0] == MATRIX_WIDTH - 1:
-    print("Agent n hat gewonnen!")
-else:
-    print("Kein Agent hat das Ende erreicht.")
+    # switch to the other agent who should work as long as its internal time is smaller than agent 1 - the timing does not always show well in the trace because of internal time updates in pyactr, which are not always visible to the user
+    while agent1_sim.show_time() > agent2_sim._Simulation__simulation.peek():
+
+        # store stimuli /triggers
+        try:
+            old_stimulus2 = agent2_sim._Simulation__env.stimulus.copy()
+        except AttributeError:
+            pass
+
+        try:
+            # do one simulation step
+            agent2_sim.step()
+
+            # print event
+            print("AGENT2, ", agent2_sim.current_event)
+
+            # update stimulus for the other agent if they changed due to the event agent 2 carried
+            if old_stimulus2 and old_stimulus2 != agent2_sim._Simulation__env.stimulus:
+                agent1_sim._Simulation__environment_activate.succeed(
+                    value=(agent2_sim._Simulation__env.trigger, agent2_sim._Simulation__pr.env_interaction))
+                break
+
+        # if the schedule is empty, the agent has no rules to do and the action is stopped
+        except simpy.core.EmptySchedule:
+            break
